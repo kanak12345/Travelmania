30511dc83efd61813c81b0022b6c2e154be899c7
kanak12345 <67689932+kanak12345@users.noreply.github.com>
2020-09-07 10:55:40 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,19 +1,17 @@
-//********************************************** NPM Installs 
 var express=require("express");
 var app=express();
 var bodyParser=require("body-parser")
 var mongoose=require("mongoose")
+var photoRoutes = require("./photos");
+var startPage=require("./index")
 var passport=require("passport")
 var LocalStrategy=require("passport-local")
-
-//******************************************** MongoDB Server 
-mongoose.connect("mongodb://localhost:27017/travel_test",{ useNewUrlParser: true, useUnifiedTopology: true });
+var travel_photo = require("./models/schema1")
 var User=require("./models/user");
-const { session, initialize } = require("passport");
-app.use(bodyParser.urlencoded({extended:true}))
+mongoose.connect("mongodb://localhost:27017/travel_photo", {useNewUrlParser:true});
 app.set("view engine","ejs");
-
-//******************************************** Password Manager
+app.use(bodyParser.urlencoded({extended:true}))
+app.use(express.static(__dirname + "/public"));
 app.use(require("express-session")({
     secret: "Once again Rusty wins cutest dog!",
     resave: false,
@@ -25,26 +23,26 @@ passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 app.use(function(req, res, next){
-   res.locals.currentUser = req.user;
-   next();
+    res.locals.currentUser = req.user;
+    next();
+ });
+
+app.use("/photos",photoRoutes)
+app.use("/",startPage)
+app.get("/photos/:id",function(req,res){
+    travel_photo.findById(req.params.id,function(err,foundcamground){
+        if(err){
+            console.log(err);
+        }
+        else    res.render("show",{kanak:foundcamground});
+        })
+        
 });
-
-
-//******************************* ROUTES starts Here *********************************************//
-
-//***************  Home route *********************
-app.get("/",function(req,res){
-    res.render("travel")
-});
-
-//************* Register page ********************** 
 app.get("/register", function(req, res){
     res.render("register"); 
  });
 
-
-//*********** After Registration ******************
-app.post("/register", function(req, res){
+ app.post("/register", function(req, res){
      var newUser = new User({username: req.body.username});
      User.register(newUser, req.body.password, function(err, user){
          if(err){
@@ -52,32 +50,23 @@ app.post("/register", function(req, res){
              return res.render("register");
          }
          passport.authenticate("local")(req, res, function(){
-            res.redirect("/"); 
+            res.redirect("/photos"); 
          });
      });
  });
-
-
- //*************** Login Page **************************
  app.get("/login", function(req, res){
     res.render("login"); 
  });
-
- //*************** After LogIN ************************
  app.post("/login", passport.authenticate("local", 
      {
-         successRedirect: "/",
+         successRedirect: "/photos",
          failureRedirect: "/login"
      }), function(req, res){
  });
-
-
- //************* Log Out ***************************
  app.get("/logout", function(req, res){
     req.logout();
     res.redirect("/");
  });
-
  
  function isLoggedIn(req, res, next){
      if(req.isAuthenticated()){
@@ -85,10 +74,8 @@ app.post("/register", function(req, res){
      }
      res.redirect("/login");
  }
+ 
 
-
-
- //*****************************************  Listen Route ******************************************
- app.listen(3000, function() { 
+app.listen(3000, function() { 
     console.log('Server listening on port 3000'); 
   });
